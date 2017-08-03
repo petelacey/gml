d7331d467ce8debf0936c9b9dbd930560bb15fb7
Pete Lacey <pete.lacey@libertymutual.com>
2017-08-03 15:10:48 -0400

    Frack this noise.

--- lib/gml/web/endpoint.ex
+++ lib/gml/web/endpoint.ex
@@ -48,7 +48,7 @@ defmodule Gml.Web.Endpoint do
   and must return the updated configuration.
   """
   def load_from_system_env(config) do
-    port = System.get_env("PORT") || raise "expected the PORT environment variable to be set"
+    port = 5000 # System.get_env("PORT") || raise "expected the PORT environment variable to be set"
     {:ok, Keyword.put(config, :http, [:inet6, port: port])}
   end
 end
