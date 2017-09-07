36ddd583a9ebb7d9aec78d4ad601bd8fdadc68e5
Pete Lacey <pete.lacey@libertymutual.com>
2017-09-07 13:16:18 -0400

    Remove unused includes

--- lib/gml/web/views/bond_view.ex
+++ lib/gml/web/views/bond_view.ex
@@ -1,10 +1,9 @@
 defmodule Gml.Web.BondView do
-  import Number.Currency
   require Logger
 
   use Gml.Web, :view
 
   def fee(amount) do
-    number_to_currency(Decimal.mult(amount, Decimal.new(0.03)))
+    Number.Currency.number_to_currency(Decimal.mult(amount, Decimal.new(0.03)))
   end
 end
